60e45735e63a917cf45af5d8e14df65f28d4dd83
dashdashzako <d@dashdashzako.net>
2019-10-30 11:39:23 +0100

    Implement storing to ES

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import json
+import os
 from http import HTTPStatus
 from flask import Flask
 from flask import request
+from elasticsearch import Elasticsearch
+
 
 app = Flask(__name__)
 
@@ -17,8 +20,23 @@ def from_request(req):
     }
 
 
+def save_event(player_event):
+    es = Elasticsearch([os.getenv('ELASTICSEARCH_HOST')])
+    es_index = os.getenv('ELASTICSEARCH_EVENTS_INDEX')
+
+    return es.index(
+        index=f'events_{es_index}',
+        doc_type='_doc',
+        body=player_event
+    )
+
+
 @app.route('/api/logs', methods=['POST'])
 def create_player_event():
     req = json.loads(request.data)
     player_event = from_request(req)
-    return '', HTTPStatus.NO_CONTENT
+    es_response = save_event(player_event)
+    if es_response['result']:
+        return '', HTTPStatus.CREATED
+    else:
+        return '', HTTPStatus.NO_CONTENT
